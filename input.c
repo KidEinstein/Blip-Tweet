edef0b1ea607c7f1ddc93ab6ec59b3d4e7d8feeb
KidEinstein <anirudhshekh@gmail.com>
2015-03-19 20:31:59 +0530

    Fixed Bug, Added Locking

---
+++ input.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+int main (int argc, char* argv[])
+{
+    char* file = "output.txt";
+    int fd;
+    struct flock lock;
+    printf ("opening %s\n", file);
+    /* Open a file descriptor to the file. */
+    fd = open ("output.txt", O_RDONLY);
+    printf ("locking\n");
+    /* Initialize the flock structure. */
+    memset (&lock, 0, sizeof(lock));
+    lock.l_type = F_RDLCK;
+    /* Place a write lock on the file. */
+    printf("Unlock : %d", fcntl (fd, F_SETLKW, &lock));
+
+    printf ("locked; hit Enter to unlock... ");
+    /* Wait for the user to hit Enter. */
+    getchar ();
+
+    printf ("unlocking\n");
+    /* Release the lock. */
+    lock.l_type = F_UNLCK;
+    fcntl (fd, F_SETLKW, &lock);
+
+    close (fd);
+    return 0;
+}
--- streaming.py
+++ streaming.py
@@ -8,31 +8,26 @@ import time
 import json
 import fcntl
 import re
-
+import HTMLParser
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
 consumer_key="NJCjifiHUVAJZy7YWAzh7oTtt"
 consumer_secret="BIU0bilnYQpB7i0wZB4ziQHeGBr5pk2xf7Mb4hjJ3P6sNdFWi7"
-
 # After the step above, you will be redirected to your app's page.
 # Create an access token under the the "Your access token" section
 access_token="601757547-6AHTD2XJVLoir2HfRp2yvwrPREtQy3O60dre03h1"
 access_token_secret="AvBXrocbNYEAKLcvalyT6OoblkFvMiAInnZ1MePuGpoEX"
 tweets = list()
+h = HTMLParser.HTMLParser()
 class StdOutListener(StreamListener):
     """ A listener handles tweets are the received from the stream.
     This is a basic listener that just prints received tweets to stdout.
-
     """
     def on_data(self, data):
         tweets.append(data)
-        return True   
-
+        return True
     def on_error(self, status):
         print(status)
-
-    
-    
 def parse_tweet():
     global tweets
     while True:
@@ -46,28 +41,25 @@ def parse_tweet():
                 screen_name = '@' + tweet['user']['screen_name'] + " : "
                 tweet_text = tweet['text']
                 tweet_text = tweet_text.encode('ascii', 'ignore')
-                if tweet_text == '' or re.search(r'^RT|^[\n]', tweet_text):
+                if tweet_text == '' or re.search(r'^RT|^[\n]', tweet_text): 
                     #print("its a retweet")
                     i += 1
                     continue
-                print(tweet_text)
-                tweet_text.replace('\n', ' ').replace('\r', '')
-                tweet_text = re.sub(r'https?:\/\/[^\s]*', '', tweet_text)
+                tweet_text = tweet_text.replace('\n', ' ').replace('\r', '')
+                tweet_text = re.sub('https?:\/\/[^\s]*', '', tweet_text)
+                tweet_text = re.sub(' +', ' ', tweet_text)  
+                tweet_text = h.unescape(tweet_text)
                 print(screen_name)
                 print(tweet_text)
-                output_text += screen_name + tweet_text
+                output_text += screen_name + tweet_text + " "
                 i -= 1
-
             fcntl.flock(output_file.fileno(), fcntl.LOCK_EX)
             output_file.write(output_text)
-            output_file.close()
+            output_file.close()           
             if len(tweets) > 50:
                 tweets = []
-            print("$$$")
         time.sleep(5)
-
 if __name__ == '__main__':
-    
     worker = Thread(target = parse_tweet)
     worker.start()
     l = StdOutListener()
@@ -75,6 +67,4 @@ if __name__ == '__main__':
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, l)
     stream.filter(track=['basketball'])
-    
-    
     
