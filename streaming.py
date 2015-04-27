6805f6b2df6f30fa12875fea2202cb2ef088d122
KidEinstein <anirudhshekh@gmail.com>
2015-04-27 22:09:04 +0530

    Revert e7c6b58..c18f9d7
    This rolls back to commit e7c6b58d1f40cc7811aa1532598ebc5d1885d9f7.

--- streaming.py
+++ streaming.py
@@ -4,14 +4,12 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from threading import Thread
-import tweepy
 import time
 import json
 import fcntl
 import re
 import HTMLParser
 from collections import deque
-SEARCH_TERM = '#CWC15'
 # Go to http://apps.twitter.com and create an app.
 # The consumer key and secret will be generated for you after
 consumer_key="NJCjifiHUVAJZy7YWAzh7oTtt"
@@ -23,7 +21,6 @@ access_token_secret="AvBXrocbNYEAKLcvalyT6OoblkFvMiAInnZ1MePuGpoEX"
 tweets = list()
 output_text = deque()
 h = HTMLParser.HTMLParser()
-output_file = open('output.txt', 'w')
 class StdOutListener(StreamListener):
     """ A listener handles tweets are the received from the stream.
     This is a basic listener that just prints received tweets to stdout.
@@ -62,37 +59,11 @@ def parse_tweet():
             del tweets[:-10]
         time.sleep(5)
 if __name__ == '__main__':
+    worker = Thread(target = parse_tweet)
+    worker.start()
     l = StdOutListener()
     auth = OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    
-##    api = tweepy.API(auth)
-##    max_tweets = 10
-##    searched_tweets = [status for status in tweepy.Cursor(api.search, q=SEARCH_TERM).items(max_tweets)]
-##    for tweet in searched_tweets:
-##        tweet_text = tweet.text
-##        if tweet_text == '' or re.search(r'^RT|^\n', tweet_text):
-##            continue
-##        screen_name = '@' + tweet.user.screen_name + " : "
-##        tweet_text = tweet_text.encode('ascii', 'ignore')
-##        tweet_text = tweet_text.replace('\n', ' ').replace('\r', '')
-##        tweet_text = re.sub('https?:\/\/[^\s]*', '', tweet_text)
-##        tweet_text = re.sub(' +', ' ', tweet_text)  
-##        tweet_text = h.unescape(tweet_text)
-##        print(screen_name)
-##        print(tweet_text)
-##        output_text.append(screen_name + tweet_text + " ")
-##        if len(output_text) > 5:
-##            output_text.popleft()
-##    fcntl.flock(output_file.fileno(), fcntl.LOCK_EX)
-##    output_file.writelines(output_text)
-##    output_file.close()           
-##    if len(tweets) > 50:
-##        del tweets[:-10]
-##    time.sleep(5)
-        
-    worker = Thread(target = parse_tweet)
-    worker.start()
     stream = Stream(auth, l)
-    stream.filter(track=[SEARCH_TERM])
+    stream.filter(track=['tazo'])
     
