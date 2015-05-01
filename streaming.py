d6134572cc31d08968c1b3b0cc06e15a9d561003
KidEinstein <anirudhshekh@gmail.com>
2015-05-01 23:41:25 +0530

    Bug fixes

--- streaming.py
+++ streaming.py
@@ -35,51 +35,62 @@ def check_tag():
     global search_term
     global tweets
     print(search_term)
-    fetched_data = requests.get('http://results.zz.mu/hashtag.txt')
+    try:
+        fetched_data = requests.get('http://results.zz.mu/hashtag.txt')
+    except:
+        return;
     print(fetched_data.text)
     print(fetched_data.text)
+    if not stream.running:
+        time.sleep(5)
+        stream.filter(track=[search_term], async=True)
     if fetched_data.text != search_term:
         print('Killed it!')
         search_term = fetched_data.text
         tweets = []
-        stream.disconnect()
-        time.sleep(5)
-        stream.filter(track=[search_term], async=True)
+        if stream.running:
+            stream.disconnect()
+            time.sleep(5)
+            stream.filter(track=[search_term], async=True)
+            time.sleep(5)
     
 
 
 def parse_tweet():
     global tweets
     while True:
-        check_tag();
-        output_file = open('output.txt', 'w')
-        while tweets:
-            item = tweets.pop()
-            tweet = json.loads(item)
-            tweet_text = tweet['text']
-            
-            if tweet_text == '' or re.search(r'^RT|^\n', tweet_text):
-                continue
-            
-            screen_name = '@' + tweet['user']['screen_name'] + " : "
-            tweet_text = tweet_text.encode('ascii', 'ignore')
-            tweet_text = tweet_text.replace('\n', ' ').replace('\r', '')
-            tweet_text = re.sub('https?:\/\/[^\s]*', '', tweet_text)
-            tweet_text = re.sub(' +', ' ', tweet_text)  
-            tweet_text = h.unescape(tweet_text)
-            print(screen_name)
-            print(tweet_text)
-            output_text.append(screen_name + tweet_text + " ")
-            
-            if len(output_text) > 5:
-                output_text.popleft()
+        try:
+            check_tag();
+            output_file = open('output.txt', 'w')
+            while tweets:
+                item = tweets.pop()
+                tweet = json.loads(item)
+                tweet_text = tweet['text']
                 
-        fcntl.flock(output_file.fileno(), fcntl.LOCK_EX)
-        output_file.writelines(output_text)
-        output_file.close()           
-        if len(tweets) > 50:
-            del tweets[:-10]
-        time.sleep(5)
+                if tweet_text == '' or re.search(r'^RT|^\n', tweet_text):
+                    continue
+                
+                screen_name = '@' + tweet['user']['screen_name'] + " : "
+                tweet_text = tweet_text.encode('ascii', 'ignore')
+                tweet_text = tweet_text.replace('\n', ' ').replace('\r', '')
+                tweet_text = re.sub('https?:\/\/[^\s]*', '', tweet_text)
+                tweet_text = re.sub(' +', ' ', tweet_text)  
+                tweet_text = h.unescape(tweet_text)
+                print(screen_name)
+                print(tweet_text)
+                output_text.append(screen_name + tweet_text + " ")
+                
+                if len(output_text) > 5:
+                    output_text.popleft()
+                    
+            fcntl.flock(output_file.fileno(), fcntl.LOCK_EX)
+            output_file.writelines(output_text)
+            output_file.close()           
+            if len(tweets) > 50:
+                del tweets[:-10]
+            time.sleep(1)
+        except:
+            pass
 
         
 if __name__ == '__main__':
